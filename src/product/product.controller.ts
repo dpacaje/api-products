ed3013bcc86f44c6a2167a2aa4c4433bdfa8dfe6
= <=>
2022-05-13 10:18:00 -0400

    Implemented Swagger

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Post, Res, HttpStatus, Body, Param, NotFoundException, Delete, Query, Put } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateProductDTO } from './dto/product.dto'
 import { ProductService } from './product.service'
+import { ProductSchema } from './schemas/product.schema';
 
+@ApiBearerAuth()
+@ApiTags('products')
 @Controller('product')
 export class ProductController {
 
     constructor(private productService:ProductService){}
 
     @Post('/create')
+    @ApiOperation({summary: 'Product created'})
+    @ApiResponse({status: 403, description: 'Forbidden.'})
     async createPost(@Res() res, @Body() createProductDTO:CreateProductDTO){
         // console.log(createProductDTO);
         const product = await this.productService.createProduct(createProductDTO);
@@ -26,6 +32,10 @@ export class ProductController {
     }
 
     @Get('/:id')
+    @ApiResponse({
+        status: 200,
+        description: 'Product not found'
+    })
     async getProduct(@Res() res, @Param('id') id:string){
         const product = await this.productService.getProduct(id);
         if (!product) throw new NotFoundException('Product not found.');
